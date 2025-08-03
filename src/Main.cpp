654fad62958953416060de9617d8ba0724d2d55d
MonkeyManCpp <sum8tkumar39699@gmail.com>
2025-08-03 14:19:33 +0530

    starter

---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main(){
+    // what we wznt to initialize from sdl like we can do mouse, keyboard or any hardware
+    SDL_Init(SDL_INIT_EVERYTHING);
+    return 0;
+}
