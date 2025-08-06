9d3dc48c09e197d1e9e2b1b52649949b0249aad8
MonkeyManCpp <sum8tkumar39699@gmail.com>
2025-08-07 03:36:23 +0530

    Successfully created windows with red color only with esc key to exit window

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,9 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game(){
+    isRunning = false;
     std::cout<<"Game Constructor called!"<<std::endl;
 }
 
@@ -10,15 +12,50 @@ Game::~Game(){
 }
 
 void Game::Initialize(){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        std::cerr<<"Error Initializing SDL."<<std::endl;
+        return;
+    }
+    // if all ok then we will create window in operating system
+    window = SDL_CreateWindow(NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
 
+    if(!window){
+        std::cerr<<"Error creating SDL window"<<std::endl;
+        return;
+    }
+
+    renderer = SDL_CreateRenderer(window ,-1,0);
+    if(!renderer){
+        std::cerr<<"Error Creating SDl renderer."<<std::endl;
+        return;
+    }
+    isRunning = true;
 }
 
-void Game::Run(){
-
-}
 
 void Game::ProcessInput(){
-
+    // pressing the esc key means leaving the window
+    SDL_Event sdlEvent;
+    // this will include all the addresses of sdlEvent
+    while(SDL_PollEvent(&sdlEvent)){
+        switch(sdlEvent.type){
+            // every time when user try to close window
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if(sdlEvent.key.keysym.sym == SDLK_ESCAPE){
+                    isRunning = false;
+                }
+                break;
+        }
+    }                
 }
 
 void Game::Update(){
@@ -26,9 +63,22 @@ void Game::Update(){
 }
 
 void Game::Render(){
+    SDL_SetRenderDrawColor(renderer,255,0,0,255);
+    SDL_RenderClear(renderer);
 
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Run(){
+    while(isRunning){
+        ProcessInput();
+        Update();
+        Render();
+    }
 }
 
 void Game::Destroy(){
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
